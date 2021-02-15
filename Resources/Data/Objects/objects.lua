aab08befff678a85eb4a03b43b575f26f1c1cf17
craigag@protonmail.com <craigag@protonmail.com>
2021-02-15 13:34:24 +0000

    fixed typo in objects.lua file.

--- Resources/Data/Objects/objects.lua
+++ Resources/Data/Objects/objects.lua
@@ -8,7 +8,7 @@ rusty_sword = {
     {
         tilesheet = "Cooz-curses-14x16.png", 
         clip_x = 15, clip_y = 2, 
-        width = 14, height = 16, depth = 1, -- depth = 10 ensures the player will be drawn above everything else
+        width = 14, height = 16, depth = 1,
         red = 187, green = 170, blue = 153,
     };
 }
