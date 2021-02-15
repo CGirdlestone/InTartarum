fb5ef0dc5bcc5e2d0e262f4a685cd60228e27412
craigag@protonmail.com <craigag@protonmail.com>
2021-02-15 12:27:37 +0000

    Tidied up lua data structures. Each entity now has it's own table as opposed to being a sub table in a larger table as it makes the loading straight forward.

--- Resources/Data/Monsters/mobs.lua
+++ Resources/Data/Monsters/mobs.lua
@@ -1,19 +1,17 @@
-mobs = {
-    bat = {
-        ["actor"] = {name = "bat", description = "You wouldn't believe one of these could grind civilization to halt with a simple virus!"};
-        ["ai"] = {state = "neutral", blind = true, scent = false};
-        ["sprite"] = {
-            tilesheet = "Cooz-curses-14x16.png", 
-            clip_x = 2, clip_y = 6, 
-            width = 14, height = 16, depth = 10, -- depth = 10 ensures the player will be drawn above everything else
-            red = 100, green = 149, blue = 237,
-        }; 
-        ["fighter"] = {hp = 10, defence = 3, attributes = { 10, 10, 10, 10, 10, 10 }};
-        -- status effects
-            -- bleeding - damage per turn
-            -- stunned - miss turns
-            -- exhausted - actions cost more energy 
-            -- poisoned - poison damage per turn
-            -- burned - fire damage per turn
-    },
+bat = {
+    ["actor"] = {name = "bat", description = "You wouldn't believe one of these could grind civilization to halt with a simple virus!"};
+    ["ai"] = {state = "neutral", blind = true, scent = false};
+    ["sprite"] = {
+        tilesheet = "Cooz-curses-14x16.png", 
+        clip_x = 2, clip_y = 6, 
+        width = 14, height = 16, depth = 10, -- depth = 10 ensures the player will be drawn above everything else
+        red = 100, green = 149, blue = 237,
+    }; 
+    ["fighter"] = {hp = 10, defence = 3, attributes = { 10, 10, 10, 10, 10, 10 }};
+    -- status effects
+        -- bleeding - damage per turn
+        -- stunned - miss turns
+        -- exhausted - actions cost more energy 
+        -- poisoned - poison damage per turn
+        -- burned - fire damage per turn
 }
--- Resources/Data/Objects/objects.lua
+++ Resources/Data/Objects/objects.lua
@@ -1,12 +1,14 @@
 
-objects = {
-    rusty_sword = {
-        ["item"] = {name = "sword", description = "A rusty iron sword."};
-        ["sprite"] = {tilesheet = "Cooz-curses-14x16.png", 
-                    clip_x = 15, clip_y = 2, 
-                    width = 14, height = 16, depth = 1, -- depth = 10 ensures the player will be drawn above everything else
-                    red = 187, green = 170, blue = 153,
-        };
-
-    }
+rusty_sword = {
+    item = 
+    {
+        name = "sword", description = "A rusty iron sword."
+    };
+    sprite = 
+    {
+        tilesheet = "Cooz-curses-14x16.png", 
+        clip_x = 15, clip_y = 2, 
+        width = 14, height = 16, depth = 1, -- depth = 10 ensures the player will be drawn above everything else
+        red = 187, green = 170, blue = 153,
+    };
 }
